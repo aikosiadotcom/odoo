022be65c3273025570b4dd3fc78269c45dd1814c
Stefan Wisełka <stefanwiselka@gmail.com>
2022-01-13 23:22:55 +0100

    Remove current_company
    current_company from class UserCompanies

--- lib/src/model/user_companies.dart
+++ lib/src/model/user_companies.dart
@@ -5,11 +5,10 @@ part 'user_companies.g.dart';
 
 @JsonSerializable()
 class UserCompanies {
-  final List<dynamic> current_company;
   final List<List<dynamic>> allowed_companies;
 
   UserCompanies(
-      {required this.current_company, required this.allowed_companies});
+      {required this.allowed_companies});
 
   factory UserCompanies.fromJson(Map<String, dynamic> json) =>
       _$UserCompaniesFromJson(json);
